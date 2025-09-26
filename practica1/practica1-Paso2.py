a64c057364e325da708665d7b880958124dbb6b9
Carlos García Arenal <cga416@alumnos.unican.es>
2025-09-26 16:26:36 +0200

    Paso 2

---
+++ practica1/practica1-Paso2.py
@@ -0,0 +1,43 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+def joinAndIgnore(G,):
+    newGraph = nx.Graph()
+    V = G.nodes()
+    for v in V:
+        predecessors = G.predecessors(v)
+        for p in predecessors:
+            newGraph.add_edge(v,p)
+            for next in predecessors:
+                newGraph.add_edge(p,next)
+                newGraph.add_edge(v, next)
+    return newGraph
+
+G = nx.DiGraph()
+elist = [("A", "C"), ("B", "C"), ("C", "E"), ("E", "F"), ("C", "D"), ("D", "F")]
+G.add_edges_from(elist)
+
+nx.draw(G, with_labels=True, node_color="lightblue", node_size=2000, font_size=14)
+plt.show()
+
+G = joinAndIgnore(G)
+nx.draw(G, with_labels=True, node_color="lightblue", node_size=2000, font_size=14)
+plt.show()
+
+
+
+
+
+#######################################################
+### PASO 2 ES HACER QUE EL GRAFO SEA NO DIRIGIDO Y
+### UNIR PADRES CON HIJOS EN COMUN
+########################################################
+
+## Recibe el grafo
+# Recorre todos los vertices del grafo. Para cada vertice mira si varios
+# varios nodos conectan hacia el en esa direccion (solo aristas direccionadas), si lo hacen crea 
+# un enlace no dirigido entre esos nodos (padres)
+
+
+# habra que mirar is es los dos que son predecessors, ya son predecessors y sucessors entre ellos
+# Primero uno los predecessor y luego recorro de nuevo y lo paso a no dirigido

bb41f8ba61e59ef44e76e39c4eda38eab31714db
Mario Cuesta <mcr807@alumnos.unican.es>
2025-10-01 10:01:44 +0200

    Paso 1 mejorado

--- practica1/practica1-Paso1.py
+++ practica1/practica1-Paso1.py
@@ -11,11 +11,26 @@ G.add_edges_from(edges)
 #nx.draw(G, with_labels=True)
 #plt.show()
 
+def remove_leaves(G, X, Y, Z):
+    check_set = Z.union(X, Y)
+    to_explore = set(G.nodes) # Conjunto de nodos a explorar
+    while to_explore:
+        node = to_explore.pop()
+        # Si el nodo no es X ni Y, no pertenece a Z y no tiene sucesores
+        if not any(G.successors(node)) and node not in check_set:
+            # Actualizamos el conjunto de nodos a explorar con los padres
+            # del nodo hoja. Eliminamos el nodo hoja.
+            predecessors = list(G.predecessors(node))
+            G.remove_node(node)
+            to_explore.update(predecessors)
 
+
+######################
+#########OLD##########
+######################
 # n = number of nodes
 # m = number of edges
-# 
-def remove_leaves(G, X, Y, Z):
+def remove_leaves_old(G, X, Y, Z):
    check_set = Z.union(X,Y) 
    to_explore = list(G.nodes) # O(n)
    while to_explore: # O(2n) -> O(n)
@@ -28,6 +43,7 @@ def remove_leaves(G, X, Y, Z):
 
 # (O(n) + O(n)) * (O(n) + O(n) + O(m)) = O(n*(n+m))
 
+######################
 
 remove_leaves(G,"A","B",set(["C"]))
 nx.draw(G, with_labels=True)
